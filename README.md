1a39b5a0b7141b15cfc7b22caf3f16cc37acb8cf
Mateus Belli <mateusbelli@hotmail.com>
2022-10-06 20:25:21 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,23 @@
+# Autores
+- Ana Karolina
+- Isabela Santos
+- Mateus Belli
+- Ruth Gomes
+- Yohana
+
+# Descrição
+O código foi desenvolvido em NodeJS, então o professor precisa ter as dependências do Node no computador para poder faze-lo funcionar.
+
+# Como rodar
+Existem 5 funções criadas:
+describe
+add (Faz a adição entre 10 e 20)
+subtract (Faz a subtração entre 20 e 10)
+multiply (Faz a multiplicação entre 20 e 10)
+divide (Faz a divisão entre 30 e 15)
+
+Todas elas vão até http://www.dneonline.com/calculator.asmx (foi utilizada a url 
+http://www.dneonline.com/calculator.asmx?WSDL para adquirir as funções) e retornam
+para o terminal a resposta.
+
+Para rodar cada uma das funções, chamar `npm run {função}`. Por exemplo `npm run add`
---
+++ index.js
@@ -0,0 +1,76 @@
+const soap = require('soap')
+const url = 'http://www.dneonline.com/calculator.asmx?WSDL'
+
+if (process.argv.includes('describe')) {
+    describe()
+} else if (process.argv.includes('add')) {
+    add(10, 20)
+} else if (process.argv.includes('subtract')) {
+    subtract(10, 20)
+} else if (process.argv.includes('multiply')) {
+    multiply(20, 10)
+} else if (process.argv.includes('divide')) {
+    divide(30, 15)
+}
+
+function describe() {
+    soap.createClient(url, (err, client) => {
+        if (err) console.error(err)
+    
+        console.log(client.describe().Calculator.CalculatorSoap)
+    })
+}
+
+function add(intA, intB) {
+    soap.createClient(url, (err, client) => {
+        if (err) console.error(err)
+    
+        client.Add({ intA, intB }, (err, result) => {
+            if (err) return console.log(err)
+    
+            console.log(result.AddResult)
+        })
+    })
+}
+
+function subtract(intA, intB) {
+    soap.createClient(url, (err, client) => {
+        if (err) console.error(err)
+    
+        client.Subtract({ intA, intB }, (err, result) => {
+            if (err) return console.log(err)
+    
+            console.log(result.SubtractResult)
+        })
+    })
+}
+
+function multiply(intA, intB) {
+    soap.createClient(url, (err, client) => {
+        if (err) console.error(err)
+    
+        client.Multiply({ intA, intB }, (err, result) => {
+            if (err) return console.log(err)
+    
+            console.log(result.MultiplyResult)
+        })
+    })
+}
+
+function divide(intA, intB) {
+    soap.createClient(url, (err, client) => {
+        if (err) console.error(err)
+    
+        client.Divide({ intA, intB }, (err, result) => {
+            if (err) return console.log(err)
+    
+            console.log(result.DivideResult)
+        })
+    })
+}
+
+// describe()
+// add(10, 20)
+// subtract(10, 20)
+// multiply(20, 10)
+// divide(20, 20)
